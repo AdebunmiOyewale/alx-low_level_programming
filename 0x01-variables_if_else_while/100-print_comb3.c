e3101bf626f45e990afc52dece3cc2bda85a6312
Adebunmi Oyewale <adebunmioyewale1@gmail.com>
2022-10-18 04:17:18 -0700

    Task 100 edits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Print all possible combination of two different digit,
+ * main - Print all possible combination of two different digits,
  * in ascending order, separated by a comma, followed by a space.
  *
  * Return: Always 0 (success).
@@ -13,7 +13,7 @@ int main(void)
 	for (digit1 = 0; digit 1 < 9; digit1++)
 	{
 
-		for (digit2 = 0; digit1 + 1; digit2 < 10; digit2++)
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
