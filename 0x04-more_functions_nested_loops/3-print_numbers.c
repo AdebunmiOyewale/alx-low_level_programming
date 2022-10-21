f6d3bc288bcc755f97d0cfddfd083bd2008bec09
Adebunmi Oyewale <adebunmioyewale1@gmail.com>
2022-10-21 07:10:43 -0700

    Task 3 edit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_numbers - Print the numbers, from 0 to 9
+ * print_numbers - Print the numbers, from 0 to 9.
  *
- * Return: void
+ * Return: print to stdout the alphabet in lowercase, followed by a new line.
  */
 
 void print_numbers(void)
@@ -11,8 +11,8 @@ void print_numbers(void)
 	int i;
 
 	for (i = '0'; i <= '9'; i++)
-	{
+
 		_putchar(i + '0');
-	}
+
 	_putchar ('\n');
 }
