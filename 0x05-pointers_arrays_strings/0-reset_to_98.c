7307c4f5650e89aa3bff27e95fea2ed5451e3122
Adebunmi Oyewale <adebunmioyewale1@gmail.com>
2022-10-24 10:20:37 -0700

    Task 0 - 101

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to an int
+ * @n: the int to check
+ *
+ * Return: 0 on success
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
