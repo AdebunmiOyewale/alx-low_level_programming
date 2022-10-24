ef4d43a8ce357afd746e4f0c4eaf04d9c064d4c0
Adebunmi Oyewale <adebunmioyewale1@gmail.com>
2022-10-24 11:01:34 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - that returns the length of a string.
+ * @s: the character
+ *
+ * Return: lenght
+ */
+int _strlen(char *s)
+{
+	int lenght = 0;
+
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
