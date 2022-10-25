770da4a6a758473e876f159ab957f7d1f6d6474e
Adebunmi Oyewale <adebunmioyewale1@gmail.com>
2022-10-25 09:09:24 -0700

    edit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,9 +13,16 @@ void print_array(int *a, int n)
 	int j;
 
 	for (j = 0; j < 0; j++)
+	{
+
 		if (j != n - 1)
+		{
 			printf("%d, ", a[j]);
+		}
 		else
+		{
 			printf("%d", a[j]);
+		}
+	}
 	printf("\n");
 }
