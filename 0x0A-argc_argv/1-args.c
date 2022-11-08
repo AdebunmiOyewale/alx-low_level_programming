2d9f7de1dd21b3b93907712950bb6f498fd28873
Adebunmi Oyewale <adebunmioyewale1@gmail.com>
2022-11-08 06:59:48 -0800

    updated

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main -  prints the number of arguments passed into it
@@ -12,7 +13,8 @@
 int main(int argc, char *argv[] __attribute__((unused)))
 {
 	if (argc > 0)
-		
+
 		printf("%d\n", argc - 1);
+
 	return (0);
 }
